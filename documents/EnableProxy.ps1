0ac8dbaea9c34709ae8d12db3ac70f016e983935
Shaun S. Wildman <SWildman@casfs.co.uk>
2019-03-19 17:02:10 +0000

    Enable Proxy Powershell

---
+++ documents/EnableProxy.ps1
@@ -0,0 +1,6 @@
+﻿$reg = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+$settings = Get-ItemProperty -Path $reg
+$settings.ProxyEnable
+
+Set-ItemProperty -Path $reg -Name ProxyEnable -Value 1
